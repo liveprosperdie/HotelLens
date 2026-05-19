01a407499bf8a867ce295b8127fbb3db787a785a
Shrey <liveprosperdie7@gmail.com>
2026-05-19 18:22:19 +0530

    Getting hotels

--- hotellens/hotels/search.py
+++ hotellens/hotels/search.py
@@ -11,11 +11,49 @@ def get_dest_id(city):
 	    "Content-Type": "application/json"
     }
     response = requests.get(url, headers=headers, params=querystring)
+    if (response.json()["data"][0]["city_name"]).upper()==city.upper():
+        return response.json()["data"][0]["dest_id"]
+    else:
+        return None
+    
+
+def get_hotels(city):
+    dest_id=get_dest_id(city)
+    url = "https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels"
+    querystring = {
+        "dest_id":dest_id,
+        "search_type":"CITY",
+        "arrival_date":"2026-06-01",
+        "departure_date":"2026-06-04",
+        "adults":"1",
+        "children_age":"0,17",
+        "room_qty":"1",
+        "page_number":"1",
+        "units":"metric",
+        "temperature_unit":"c",
+        "languagecode":"en-us",
+        "currency_code":"INR",
+        "location":"US"}
+    headers = {
+	    "x-rapidapi-key": "392ab4de0bmsh54ce54dab743b94p161e0ejsn2fe9990ac38c",
+	    "x-rapidapi-host": "booking-com15.p.rapidapi.com",
+	    "Content-Type": "application/json"
+    }
+    
+    response = requests.get(url, headers=headers, params=querystring)
     data=response.json()
-    good_data=json.dumps(data, indent=4)
-    print(good_data)
+    hotels_details=[]
+    for hotel in data["data"]["hotels"]:
+        hotels_details.append(
+            {
+                "name": hotel["property"]["name"],
+                "review_score": hotel["property"]["reviewScore"],
+                "price": hotel["property"]["priceBreakdown"]["grossPrice"]["value"],
+                "property_class": hotel["property"]["propertyClass"],
+                "hotel_id": hotel["hotel_id"]
+            }
+        )
+    print(json.dumps(hotels_details,indent=4))
 
-def main():
-    get_dest_id("Agra")
 
-main()
+get_hotels("AGRA")
